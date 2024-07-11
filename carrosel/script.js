2b55da3ccf117dc7487028e66e0c22171985442a
Donizet Silva <61122214+donizeti26@users.noreply.github.com>
2024-07-11 09:02:20 -0300

    adicionando js

--- carrosel/script.js
+++ carrosel/script.js
@@ -1,8 +1,33 @@
-let listOne = document.querySelectorAll('#listOne li')
-let listTwo = document.querySelectorAll('#listTwo li')
+let listOne = document.querySelectorAll('#lista1 li')
+let listTwo = document.querySelectorAll('#lista2 li')
 
 let numVideos = document.querySelectorAll('.numVideos')
-
+let numDeItens = 6
 
 numVideos[0].textContent = listOne.length
 numVideos[1].textContent = listTwo.length
+
+function show(indice, indiceLista){
+
+    let listaUl = document.querySelector('#lista'+indiceLista)
+
+    let mover = 550
+    let posicaoXDireita = mover
+    let posicaoXEsquerda = -mover
+
+
+
+    if(indice == +1){
+        listaUl.scrollBy({
+            left: posicaoXDireita,
+            behavior: 'smooth'
+        });
+    }
+    if(indice == -1){
+        listaUl.scrollBy({
+            left: posicaoXEsquerda,
+            behavior: 'smooth'
+        });
+    }
+    console.log('aaaaaa')
+}
